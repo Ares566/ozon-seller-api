203023fbdd16a80f648399506588eaaf627c986a
Renat Abaidulin <ares566@ya.ru>
2021-10-24 13:26:02 +0300

    Add GetUnfulfilledList method

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"ozon-seller-api/application"
+	"ozon-seller-api/infrastructure/logger"
 	"ozon-seller-api/internal/app"
+	"github.com/robfig/cron"
+
 )
 
 
@@ -12,16 +15,16 @@ func main() {
 		appData   = app.New()
 		fbsApp     = appData[app.FBSApp].(*application.FBSApplication)
 	)
-	fbsApp.GetUnfulfilledList()
 
-	//c := cron.New()
-	//// раз в 3 минуты
-	//cacheError := c.AddFunc("0 */3 * * * *", func() { fbsApp.GetUnfulfilledList() })
-	//if cacheError != nil {
-	//	logger.Error("Ошибка запуска обновления по расписанию")
-	//	return
-	//}
-	//c.Start()
+
+	c := cron.New()
+	// раз в 3 минуты
+	cacheError := c.AddFunc("0 */3 * * * *", func() { fbsApp.GetUnfulfilledList() })
+	if cacheError != nil {
+		logger.Error("Ошибка запуска обновления по расписанию")
+		return
+	}
+	c.Start()
 
 
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.3
+	github.com/robfig/cron v1.2.0
 	github.com/sirupsen/logrus v1.8.1
 )
 
